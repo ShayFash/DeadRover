e080fe1f95140401203fd4fe2f4772f77c8881c6
alehen <alec_henry@outlook.com>
2022-02-25 16:06:45 -0600

    fix(unit): can be attacked after switching and does not wait to be eliminated

--- RedRover/Assets/Scripts/GenericUnit.cs
+++ RedRover/Assets/Scripts/GenericUnit.cs
@@ -10,11 +10,13 @@ public abstract class GenericUnit : MonoBehaviour
     public int Attack;
     public int Health;
     public int MaxHealth { get; protected set; }
+    protected int InitialMaxHealth;
 
     [SerializeField]
     protected int SwitchSidesCountdown;
     protected int NumTurnsToSwitchSides;
     protected int NumTimesSwitched = 0;
+    protected int MaxAllowedSwitches = 3;
     protected bool SwitchingSides;
 
 
@@ -27,6 +29,7 @@ public abstract class GenericUnit : MonoBehaviour
     protected void Init()
     {
         MaxHealth = Health;
+        InitialMaxHealth = MaxHealth;
         NumTurnsToSwitchSides = SwitchSidesCountdown;
 
         Controller = GameObject.FindGameObjectWithTag("GameController").GetComponent<Controller>();
@@ -55,12 +58,18 @@ public abstract class GenericUnit : MonoBehaviour
         Health = Mathf.Max(0, Health - value);
         if (Health == 0)
         {
+            if (NumTimesSwitched == MaxAllowedSwitches)
+            {
+                gameObject.SetActive(false);
+                return;
+            }
             // TODO: update visually
             SwitchingSides = true;
             SwitchSidesCountdown = NumTurnsToSwitchSides;
             Debug.Log("I'm starting to switch sides!");
 
             CountdownText.text = SwitchSidesCountdown.ToString();
+            CountdownText.color = CompareTag("Living") ? Color.black : Color.white;
             CountdownText.enabled = true;
         }
     }
@@ -82,21 +91,18 @@ public abstract class GenericUnit : MonoBehaviour
 
         if (SwitchSidesCountdown <= 0)
         {
+            SwitchingSides = false;
             NumTimesSwitched++;
             tag = CompareTag("Living") ? "Dead" : "Living";
             Sprite.color = CompareTag("Living") ? Color.white : Color.black;
 
-            MaxHealth = Mathf.RoundToInt(MaxHealth * (1 - (0.25f * NumTimesSwitched)));
+            Debug.Log(NumTimesSwitched / (MaxAllowedSwitches + 1f));
+            MaxHealth = Mathf.RoundToInt(InitialMaxHealth * (1 - (NumTimesSwitched / (MaxAllowedSwitches + 1f))));
             Health = MaxHealth;
 
             CountdownText.enabled = false;
 
             Debug.Log("I've switched sides");
-
-            if (MaxHealth == 0)
-            {
-                gameObject.SetActive(false);
-            }
         }
     }
 
